50c0c46edea5f6ef462f1a0351e8f9b2d11dc469
chanel <chaneldesirae18@gmail.com>
2020-05-02 09:01:48 -0500

    changes to index.html and jss files other codes not functional

--- script.js
+++ script.js
@@ -1,44 +1,95 @@
-<!DOCTYPE html>
-<html lang="en-us">
-<head>
-    <!-- title and standard meta usage -->
-    <title>Password Generator</title>
-    <meta charset="UTF-8" />
-    <meta name="description" content="" />
-    <meta name="keywords" content="" />
-    <!-- link to our style sheet -->
-    <link rel="stylesheet" href="" />
-</head>
+    
+    function generatePassword() {
+    
+// Special characters for the function created
 
-<body>
-    <script type="text/javascript">
+var lowercase  = 'abcdefghijklmnopqrstuvwxyz';
+var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXY';
+var numbers = '0123456789';
+var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
+var values = 'lowercase + uppercase + numbers + symbols';
+var password = '';
+
+// Prompts that come up after you click generate password
+function generatePassword() {
+
+  alert("Welcome to Password Generator");
+
+  varconfirmPassword = confirm("Please select the length of your password greater than 8 values.");
+
+  varconfirmNumbers = confirm("Do you want numbers in your password?");
+
+  varconfirmLowerCase = confirm("Do you want lowercases in your password?");
+
+  varconfirmUppercase = confirm("Do you want uppercases in your password?");
+
+  varconfirmSymbols = confirm("Do you want special characters in your password?");
+
+} else if (upperCase && lowerCase && special) {
+  characters = upper.concat(lower, symbol);
+
+} else if (upperCase && lowerCase && numbers) {
+  characters = upper.concat(lower, num);      
+
+} else if (upperCase && special && numbers) {
+  characters = upper.concat(special, num);
+
+} else if (lowerCase && special && numbers) {
+  characters = lower.concat(special, num);
 
 
-String[] symbols = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
-int length = 10;
-Random random = SecureRandom.getInstanceStrong();    // as of JDK 8, this should return the strongest algorithm available to the JVM
-StringBuilder sb = new StringBuilder(length);
-for (int i = 0; i < length; i++) {
-    int indexRandom = random.nextInt(symbols.length);
-    sb.append(symbols[indexRandom]);
+// For passwords containing only two characters
+} else if (upperCase && lowerCase) {
+  characters = upper.concat(lower);
+
+} else if (upperCase && special) {
+  characters = upper.concat(symbol);
+
+} else if (upperCase && numbers) {
+  characters = upper.concat(num);
+
+} else if (lowerCase && special) {
+  characters = lower.concat(symbol);
+
+} else if (lowerCase && numbers) {
+  characters = lower.concat(num);
+
+} else if (special && numbers) {
+  characters = symbol.concat(number);
+
+
+// For passwords containing only one symbol
+} else if (upperCase) {
+  characters = upper;
+
+} else if (lowerCase) {
+  characters = lower;
+
+} else if (special) {
+  characters = symbol;
+
+} else if (numbers) {
+  characters = num;
+};
+
+
+///Loop
+var pw = ''
+for (vari = 0; i <= length; i++) {
+{password = password + values.charAt(Math.floor(Math.random() * Math.floor(value.length - 1)));
 }
-String password = sb.toString();
-
-function generate(length = 12) {
-    var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    var lowercase = 'abcdefghijklmnopqrstuvwxyz';
-    var numbers = '0123456789';
-    var symbols = '!"#$%&\'()*+,-./:;<=>?@^[\\]^_`{|}~';
-    var all = uppercase + lowercase + numbers + symbols;
-    var password = '';
-    for (var index = 0; index < length; index++) {
-        var character = Math.floor(Math.random() * all.length);
-        password += all.substring(character, character + 1);
-    }
+///
+    var password = pw.join("");
     return password;
 }
+var generateBtn = document.querySelector("#generate");
 
-    <script> 
-</body>
-</html>
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
+  passwordText.value = password;
+}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
